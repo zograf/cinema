f32c751e8dadb0b9fbbe7f2e0d10fab29a084bc8
zograf <lazzarmilanovic@gmail.com>
2022-04-16 16:35:43 +0200

    mongo uri visibility fix

--- main.go
+++ main.go
@@ -1,35 +1,15 @@
 package main
 
 import (
-	"context"
-	"log"
 	"net/http"
-	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/zograf/cinema/cinema"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var app *cinema.App
-
-func dbConnect() *mongo.Client {
-	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
-	clientOptions := options.Client().
-		ApplyURI("mongodb+srv://admin:admin@uptest.idmbk.mongodb.net/cinema?retryWrites=true&w=majority").
-		SetServerAPIOptions(serverAPIOptions)
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	client, err := mongo.Connect(ctx, clientOptions)
-	// ctx is actually irrelevant for querries
-	if err != nil {
-		log.Fatal(err)
-	}
-	return client
-}
 
 func get(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
@@ -49,13 +29,12 @@ func del(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
-	client := dbConnect()
+	app = cinema.CreateApp("cinema")
 
 	router.GET("/", get)
 	router.POST("/", put)
 	router.DELETE("/", del)
 
-	app = cinema.CreateApp("cinema", client)
 	router.Use(cors.Default())
 	router.Run()
 }
